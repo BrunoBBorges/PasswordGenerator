4d695e4fa476309bdc20ac32abbe497a1846cdd3
Bruno Borges <brunobborges68@gmail.com>
2020-11-14 18:06:37 -0500

    updating code

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+//declare variables
+var capitialLetters = document.getElementById("capitals").checked;
+var lowercaseLetters = document.getElementById("lowercase").checked;
+var passwordNumbers = document.getElementById("numbers").checked;
+var specialChars = document.getElementById("specialChars").checked;
+
 
 // Write password to the #password input
 function writePassword() {
@@ -9,6 +15,55 @@ function writePassword() {
   passwordText.value = password;
 
 }
+// Define actions of generate password
+function generatePassword() {
+
+  // Prompt user for password parameters
+  var charLength = prompt("Enter a number for your password length between 8 - 128 characters.")
+    // Return an error for any invalid inputs to character length
+    if (isNaN(charLength) || parseInt(charLength) < 8 || parseInt(charLength) > 128 || charLength === ""){
+      alert("Error, Please enter a number between 8-128")
+    }
+ 
+  var randompassword = "";
+  var password = "";
+//Define variables to hold alpabetical/numerical/special character values for use in generation
+  var specialChars = "!@#$%^&*()_-=+<>?;:\"''{}[]";
+  var numbers = "0123456789";
+  var lowercase = "abcdefghijklnmopqrstuvxyz";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+// check to see what the user has selected
+  if (document.getElementById("lowerCase").checked) {
+    pword = pword + lowercase;
+  }
+  
+  if (document.getElementById("upperCase").checked) {
+    pword = pword + uppercase;
+  }
+  
+  if (document.getElementById("numbers").checked) {
+    randompassword = randompassword + numbers;
+  }
+  
+  if (document.getElementById("symbols").checked) {
+    randompassword = randompassword + specialChars;
+  }
+
+
+  for (var i = 0; i < charLength; i++) {
+    var gen_random = Math.floor(Math.random() * randompassword.length);
+     password += randompassword[gen_random];
+  }
+
+  return password;
+
+
+  function newFunction() {
+    return document.getElementById("charLength").value;
+  }
+}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
